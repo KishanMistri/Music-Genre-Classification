0e90aed49f1c7de754e2fbc8d34d47508f129065
Kishan_Mistri <14bit029@nirmauni.ac.in>
2022-04-06 14:43:29 +0530

    Update ClassifierModel.py

--- ClassifierModel.py
+++ ClassifierModel.py
@@ -1,25 +1,13 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-"""
 from pyAudioAnalysis import audioTrainTest as aT
 import os
 
 dirname = '/home/kmistri/audioClassificationNew/trainingData'
 
 subdirectories = os.listdir(dirname)[:10]
-
-#subdirectories.pop(0)
-
 subdirectories = [dirname + "/" + subDirName for subDirName in subdirectories]
-
-print(subdirectories)
+# print(subdirectories)
 aT.featureAndTrain(subdirectories, 1.0, 1.0, aT.shortTermWindow, aT.shortTermStep, "svm", "newModel", False)
 
-
 '''
-(list of directories,mid window size,mid window steps, short window size, short window step,type, file name to be saved,)
-Frequancy
- 
+(list of directories,mid window size,mid window steps, short window size, short window step,type, file name to be saved,Frequency)
 '''
